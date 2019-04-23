cab130a9a72b1bc393133489a1bfc5b57f9818e6
Nuc1earDragon <1nuclear1@gmail.com>
2019-04-23 05:04:34 +0300

    menu

--- Tanki_1.js
+++ Tanki_1.js
@@ -1,23 +1,28 @@
 
                 window.onload = function() {
+
                     game = new Game();
                     map = new Map();
                     map.draw();
-                    game.context.drawImage(game.mainTank.up.main, game.mainTank.x, game.mainTank.y,30,30);
-                    
-                    window.addEventListener("keydown", mainTankMove);
-                   
-                    window.addEventListener("keydown", mainTankBullet);
-                    
-                   
-                    game.mainInterval = setInterval(function (){
-                        map.draw();
-                        game.context.drawImage(game.mainTank.position, game.mainTank.x, game.mainTank.y,30,30);
-                        bulletFly();
-                        createBot();
-                        botMove();
-                        }
-                       , 25);
-                       
                 }
+
+function startGame(){
+    game.context.drawImage(game.mainTank.up.main, game.mainTank.x, game.mainTank.y,30,30);
+                    
+    window.addEventListener("keydown", mainTankMove);
+   
+    window.addEventListener("keydown", mainTankBullet);
+    
+   
+    game.mainInterval = setInterval(function (){
+        map.draw();
+        game.context.drawImage(game.mainTank.position, game.mainTank.x, game.mainTank.y,30,30);
+        bulletFly();
+        createBot();
+        botMove();
+        }
+       , 25);
+       
+}
+
                     
